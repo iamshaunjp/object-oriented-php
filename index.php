cbfbcdc42d79253dfede5a3a167dcae2fcf3ca08
Shaun <shaun@thenetninja.co.uk>
2019-08-30 13:44:32 +0100

    lesson-8

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
     public $username;
     private $email;
+    public $role = 'member';
 
     public function __construct($username, $email){
       //$this->username = 'ken';
@@ -14,6 +15,10 @@
     public function addFriend(){
       //return "added a new friend";
       return "$this->username just added a new friend";
+    }
+
+    public function message(){
+      return "$this->username sent a new message";
     }
 
     // getters
@@ -33,10 +38,15 @@
   class AdminUser extends User {
 
     public $level;
+    public $role = 'admin';
 
     public function __construct($username, $email, $level){
       parent::__construct($username, $email);
       $this->level = $level; 
+    }
+
+    public function message(){
+      return "an admin sent a new message";
     }
 
   }
@@ -45,9 +55,11 @@
   $userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
   $userThree = new AdminUser('yoshi', 'yoshi@thenetninja.co.uk', 5);
 
-  echo $userThree->level . '<br>';
-  echo $userThree->username . '<br>';
-  echo $userThree->getEmail() . '<br>';
+  echo $userOne->role . '<br>';
+  echo $userThree->role . '<br>';
+
+  echo $userOne->message() . '<br>';
+  echo $userThree->message() . '<br>'; 
 
 ?>
 
