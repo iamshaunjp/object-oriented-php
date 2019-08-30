fa2b233783e2d959d244fbb65d74f0a27a6a2210
Shaun <shaun@thenetninja.co.uk>
2019-08-30 12:22:51 +0100

    lesson-3

--- index.php
+++ index.php
@@ -2,12 +2,32 @@
 
   class User {
 
+    public $username = 'ryu';
+    public $email = 'ryu@thenetninja.co.uk';
+
+    public function addFriend(){
+      //return "added a new friend";
+      return "$this->username just added a new friend";
+    }
+
   }
 
   $userOne = new User();
   $userTwo = new User();
 
-  echo 'the class is: ' . get_class($userOne);
+  echo $userOne->username . '<br>';
+  echo $userOne->email . '<br>';
+  echo $userOne->addFriend() . '<br>';
+
+  $userTwo->username = 'yoshi';
+  $userTwo->email = 'yoshi@thenetninja.co.uk';
+
+  echo $userTwo->username . '<br>';
+  echo $userTwo->email . '<br>';
+
+  //print_r(get_class_vars('User'));
+  print_r(get_class_methods('User'));
+
 
 ?>
 
